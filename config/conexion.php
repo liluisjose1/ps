846a1fbd7c70a7fe6f52bc45b9fda5435e43f2eb
Luis Iraheta <liluisjose1@hotmail.com>
2018-05-11 16:00:03 -0600

    Respaldo

---
+++ config/conexion.php
@@ -0,0 +1,31 @@
+<?php
+/*~ Archivo conexion.php
+.---------------------------------------------------------------------------.
+|    Software: SI - Proyeccion Social FMO                                   |
+|     Versión: 1.0                                                          |
+|   Lenguajes: PHP, HTML, CSS3 y Javascript                                 |
+| ------------------------------------------------------------------------- |
+|   Autores: Luis Iraheta                                                   |
+| Copyright (C) 2017, FMO-UES. Todos los derechos reservados.               |
+| ------------------------------------------------------------------------- |
+|                                                                           |
+| Este archivo es parte del sistema de informacion SI  para la Unidad       |
+| de proyeccion social .                                                    |
+|                                                                           |
+'---------------------------------------------------------------------------'
+*/
+?>
+<?php
+	function conectarse()
+	{
+		$servidor 	=	 "localhost";
+		$usuario 	=	 "root";
+		$password 	=	 "";
+		$bd 		=	 "ps";
+
+		$conectar = new mysqli($servidor, $usuario, $password, $bd);
+		    return $conectar;
+	}
+
+	$conexion = conectarse();
+?>
---
+++ index.php
@@ -0,0 +1,100 @@
+<?php include "template/header.php"; ?>
+ <!-- Page Header -->
+    <header class="masthead" style="background-image: url('assets/img/banner.jpg')">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+<!--             <center><img style="width: 200px;" src="assets/img/min-w.png"></center> -->
+              <h2>Universidad de El Salvador <br> Facultad Multidisciplinaria Orienta </h2>
+              <span class="subheading">Unidad de Proyección Social</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+ <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+          <h1 class="my-4">Page Heading
+            <small>Secondary Text</small>
+          </h1>
+
+          <!-- Blog Post -->
+          <div class="card mb-4">
+            <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+            <div class="card-body">
+              <h2 class="card-title">Post Title</h2>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+              <a href="#" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+            <div class="card-footer text-muted">
+              Posted on January 1, 2017 by
+              <a href="#">Start Bootstrap</a>
+            </div>
+          </div>
+
+          <!-- Blog Post -->
+          <div class="card mb-4">
+            <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+            <div class="card-body">
+              <h2 class="card-title">Post Title</h2>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+              <a href="#" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+            <div class="card-footer text-muted">
+              Posted on January 1, 2017 by
+              <a href="#">Start Bootstrap</a>
+            </div>
+          </div>
+
+          <!-- Blog Post -->
+          <div class="card mb-4">
+            <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+            <div class="card-body">
+              <h2 class="card-title">Post Title</h2>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+              <a href="#" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+            <div class="card-footer text-muted">
+              Posted on January 1, 2017 by
+              <a href="#">Start Bootstrap</a>
+            </div>
+          </div>
+
+
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+        <div class="col-md-4">
+         <!-- Side Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Facultad</h5>
+            <div class="card-body">
+<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffmoues%2F&tabs=timeline&width=300&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1584763541776868" width="300" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+            </div>
+          </div>
+          <!-- Side Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Sección de Deportes</h5>
+            <div class="card-body">
+<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FSecci%25C3%25B3n-de-Deportes-Facultad-Multidisciplinaria-Oriental-1096150557191677%2F&tabs=timeline&width=300&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1584763541776868" width="300" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+<?php include "template/footer.php"; ?>
